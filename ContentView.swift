9535f43d60e0ed32f6a2cb373ebfcfc96fde8820
MindaugasBalakauskas <mindaugas.balakauskas@gmail.com>
2025-10-28 13:13:46 +0000

    Initial commit

---
+++ ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUI Avanced Data Flow
+//
+//  Created by M Balakauskas on 28/10/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUI_Avanced_Data_FlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Avanced_Data_FlowApp.swift
+//  SwiftUI Avanced Data Flow
+//
+//  Created by M Balakauskas on 28/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Avanced_Data_FlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
